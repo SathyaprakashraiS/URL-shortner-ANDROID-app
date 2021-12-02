2aaae2c68958ded01d9c8b9c8658d10d0a6f3bfd
SATHYA <sathyaprakashrai2002@gmail.com>
2021-12-02 08:40:24 +0530

    link input added

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { FlatList, SafeAreaView, StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+  const [link, onChangelink] = React.useState(null);
+  const [number, onChangeNumber] = React.useState(null);
+
   const [book,setbook] = useState([]);
   var apiavail=false;
   async function fetchBook(stand) {
     var apiavail=false;
-    var booklink='http://127.0.0.1:8000/sbooks-list/10/';
+    var booklink='https://dope2meal.herokuapp.com/food-list/';
     const request = await fetch(booklink)
       .then(response => {
         if(response.ok)
@@ -23,49 +25,62 @@ export default function App() {
     })
       .then(data => { 
         setbook(data)
-        console.log(setbook)
+        console.log("here0")
       })
       .catch((error) => {
+        console.log("here1")
         console.log("the error ",error)
       });
     }
-    useEffect(() => {
-      fetchBook();
-    }, []);
+  //   useEffect(() => {
+  //     console.log("called func")
+  //     fetchBook();
+  //   }, []);
+  console.log("API AVAIL",apiavail)
 
+    function theprint(topr){
+      console.log("recieved link: ",topr);
+      var result = link.replace("/", "---");
+      console.log("transmitted link: ",result);
+      fetchBook();
+    }
   return (
     // <SafeAreaView>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      {apiavail ? (
-        <FlatList 
-        data={book.data}
-        showsVerticalScrollIndicator={true}
-        renderItem={({item}) =>
+      
+      
+      
+      {/* <TextInput style={styles.input} placeholder="enter link" onChangeText={onChangeText} value={text} /> */}
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangelink}
+        value={link}
+        placeholder="enter link"
+      />
+
+      {/* <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      /> */}
+
+      <Button
+        title="Press me"
+        color="#f194ff"
+        onPress={() => theprint(link)}
+      />
+      
+      {/* {
+      book.map(item => (
         <View>
           <Text>{item.name}</Text>
-          <Text>{item.author}</Text>
         </View>
-      }
-      keyExtractor={item => item._id}
-        />
-      ):(
-        <Text>BOOK ILLA ODRA</Text>
-      )
-      }
-      {/* <FlatList 
-      data={book.data}
-      showsVerticalScrollIndicator={true}
-      renderItem={({item}) =>
-      <View>
-        <Text>{item.name}</Text>
-        <Text>{item.author}</Text>
-      </View>
-    }
-    keyExtractor={item => item._id}
-      /> */}
-      {/* <Text>BOOK ILLA ODRA</Text> */}
-      
+          ))
+      } */}
+
       <StatusBar style="auto" />
     </View>
     // </SafeAreaView>
@@ -73,6 +88,13 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
